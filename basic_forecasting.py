b16448e93d8eab459272ed6013ff0eaf63ac47c4
Nde Lucien <chelucien08@gmail.com>
2022-08-12 17:03:24 +0100

    adjusting errors on linear regression algorithm

--- basic_forecasting.py
+++ basic_forecasting.py
@@ -17,19 +17,20 @@ df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume',]]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
 df['PCT_Change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
 
+#           price          x           x            x
 df = df[['Adj. Close', 'HL_PCT', 'PCT_Change', 'Adj. Volume']]
 
 forecast_col = 'Adj. Close'
 df.fillna('-99999', inplace=True)
 
-forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.1*len(df)))
 print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-X = np.array(df.drop(['label'], 1))
+X = np.array(df.drop(['label', 'Adj. Close'], 1))
 X = preprocessing.scale(X)
-X = X[:-forecast_out]
 X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
@@ -49,7 +50,7 @@ with open('linearregression.pickle', 'wb') as f:
     pickle.dump(clf, f)
 
 pickle_in = open('linearregression.pickle', 'rb')
-clf = pickle.load(clf, f)
+clf = pickle.load(pickle_in)
 
 accuracy = clf.score(X_test, y_test)
 forecast_set = clf.predict(X_lately)
