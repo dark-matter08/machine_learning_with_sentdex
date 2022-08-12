2bb7ce20b6738bed0ea007979809ae410f25ca89
Nde Lucien <chelucien08@gmail.com>
2022-08-12 16:44:16 +0100

    starting part 11

--- basiclinearregression_algorithm.py.py
+++ basiclinearregression_algorithm.py.py
@@ -16,6 +16,16 @@ def best_fit_slope_and_intercept(xs, ys):
     b = mean(ys) - m * mean(xs)
     return m, b
 
+def squared_error(ys_orig, ys_line):
+    return sum((ys_line - ys_orig) ** 2)
+
+def coefficient_of_determinantion(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+
+    return 1 - (squared_error_regr / squared_error_y_mean)
+
 m, b = best_fit_slope_and_intercept(xs, ys)
 
 regression_line = [(m*x)+b for x in xs]
@@ -23,7 +33,9 @@ regression_line = [(m*x)+b for x in xs]
 predict_x = 0
 predict_y = (m*predict_x) + b
 
-print(predict_y)
+r_squared = coefficient_of_determinantion(ys, regression_line)
+
+print(r_squared)
 
 plt.scatter(xs, ys)
 plt.scatter(predict_x, predict_y, color='g')
