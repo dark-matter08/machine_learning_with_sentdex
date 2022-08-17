b17b42a261d2a2859daabc08b6e4b87b4319dd43
Nde Lucien <chelucien08@gmail.com>
2022-08-17 15:41:16 +0100

    complete custom knn. i am customizing functions

--- classification_algorithms/custom_knn.py
+++ classification_algorithms/custom_knn.py
@@ -29,8 +29,9 @@ def k_nearest_neighbors(data, predict, k=3):
 
     votes = [i[1] for i in sorted(distances)[:k]]
     vote_result = Counter(votes).most_common(1)[0][0]
+    confidence = Counter(votes).most_common(1)[0][1] / k
 
-    return vote_result
+    return vote_result, confidence
 
 
 # result = k_nearest_neighbors(dataset, new_features, k=3)
@@ -40,14 +41,12 @@ def k_nearest_neighbors(data, predict, k=3):
 # [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
 # plt.scatter(new_features[0], new_features[1], s=100, color=result)
 # plt.show()
-
-
 df = pd.read_csv('breast-cancer-wisconsin.data')
 df.replace('?', -99999, inplace=True)
 df.drop(['id'], 1, inplace=True)
 full_data = df.astype(float).values.tolist()
 
-test_size = 0.2
+test_size = 0.4
 train_set = {2: [], 4: []}
 test_set = {2: [], 4: []}
 
@@ -65,10 +64,14 @@ total = 0
 
 for group in test_set:
     for data in test_set[group]:
-        vote = k_nearest_neighbors(train_set, data, k=5)
+        vote, confidence = k_nearest_neighbors(train_set, data, k=5)
         if group == vote:
             correct += 1
+        else:
+            # print(confidence)
+            pass
 
         total += 1
 
 print('Accuracy: ', correct/total)
+
