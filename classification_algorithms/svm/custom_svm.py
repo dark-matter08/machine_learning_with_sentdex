65724f4916600db1030fe1041febe5f300283f14
Nde Lucien <chelucien08@gmail.com>
2022-08-18 23:23:20 +0100

    tut 27

--- classification_algorithms/svm/custom_svm.py
+++ classification_algorithms/svm/custom_svm.py
@@ -41,24 +41,50 @@ class CustomSVM:
 
         # extremely expensive
         b_range_multiple = 5
-        # 
+        # we dont need to take as small steps as we do w
         b_multiple = 5
         latest_optimum = self.max_feature_value * 10
 
         for step in step_sizes:
             w = np.array([latest_optimum, latest_optimum])
-            
             # we can do this because convex
             optimized = False
-
             while not optimized:
-                pass
+                for b in np.arange(-1 * (self.max_feature_value * b_range_multiple), self.max_feature_value * b_range_multiple, step * b_multiple):
+                    for transformation in transforms:
+                        w_t = w * transformation
+                        found_option = True
+                        # weakest link in the SVM fundamentaly
+                        # SMO attempts to fix this a bit
+                        #  yi(xi.w + b) >= 1
+                        # #### maybe add a break here later
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi = i
+                                if not yi * (np.dot(w_t, xi) + b) >= 1:
+                                    found_option = False
+                                    # break
+                        if found_option:
+                            opt_dict[np.linalg.norm(w_t)] = [w_t, b]
+                
+                if w[0] < 0:
+                    optimize = True
+                    print('Optimized a step')
+                else:
+                    w = w - step
 
+            norms = sorted([n for n in opt_dict])
+            opt_choice = opt_dict[norms[0]]
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+            latest_optimum = opt_choice[0][0] + step*2
 
     def predict(self, features):
         classification = np.sign(np.dot(np.array(features), self.w) + self.b)
         
         return classification
+
+
 
 
 
